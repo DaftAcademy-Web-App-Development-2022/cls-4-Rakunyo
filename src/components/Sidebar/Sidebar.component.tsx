d86ee2fe1a65a0984baaaf017d793f5f9e0df7c5
Rakunyo <i.plachecka98@gmail.com>
2022-12-07 10:28:01 +0100

    Show user's name and mail

--- src/components/Sidebar/Sidebar.component.tsx
+++ src/components/Sidebar/Sidebar.component.tsx
@@ -4,21 +4,24 @@ import { Form, User } from "../index";
 import styles from "./Sidebar.module.css";
 
 import userData from "~/data/userData.json";
+import { useSession } from "next-auth/react";
 
 const Sidebar = () => {
   const handleLogout = (name: string, id: string) => {
     console.log("logout", name, id);
   };
 
+  const { data } = useSession();
+
   return (
     <div className={styles.root}>
       <div className={styles.top}>
         <User
           logout={() => handleLogout(userData.name, userData.id)}
-          email={userData.email}
-          name={userData.name}
-          image={userData.url}
-          loading={false}
+          email={data?.user?.email || ""}
+          name={data?.user?.name || ""}
+          image={data?.user?.image || ""}
+          loading={!data}
         />
       </div>
       <div className={styles.center}>
